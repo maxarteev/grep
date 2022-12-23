6aee4e62b2c4e87b7379c1f2611b1cde5abcd229
theoryM <silnov@agronews.com>
2022-12-23 10:59:17 +0300

    grep to libs

--- src/libs.rs
+++ src/libs.rs
@@ -1,2 +1,32 @@
+use std::fs::File;
+use std::io::Read;
 
+#[derive(Debug)]
+pub struct Data{
+    count: i32,
+    value: Vec<String>,
+}
 
+pub fn grep(
+    file_name: &str,
+    search_word: String,
+) -> Data
+{
+    let mut file = File::open(file_name).expect("file not found");
+    let mut text = String::new();
+    let mut data = Data{
+        count: 0,
+        value: vec![],
+    };
+    file.read_to_string(&mut text).expect("I can't read the file");
+    for line in text.split_terminator(&['.', '!']) {
+        for line_iter in line.split_whitespace() {
+            if line_iter.trim().to_lowercase() == search_word.trim().to_lowercase() {
+                data.value.push(line.trim().to_string() + ".");
+                data.count +=1;
+                break;
+            }
+        }
+    }
+    data
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,8 @@
-use std::fs::File;
-use std::io::Read;
-
-#[derive(Debug)]
-struct Data{
-    count: i32,
-    value: Vec<String>,
-}
-
-fn grep(
-    mut file: File,
-    search_word: String,
-) -> Data
-{
-    let mut text = String::new();
-    let mut data = Data{
-        count: 0,
-        value: vec![],
-    };
-    file.read_to_string(&mut text).expect("I can't read the file");
-    for line in text.split_terminator(&['.', '!']) {
-        for line_iter in line.split_whitespace() {
-            if line_iter.trim().to_lowercase() == search_word.trim().to_lowercase() {
-                data.value.push(line.trim().to_string() + ".");
-                data.count +=1;
-                break;
-            }
-        }
-    }
-    data
-}
+mod libs;
 
 fn main() {
     let search_word = "современные".to_string();
-    let file = File::open("file.txt").expect("don't open =(");
-    let vector = grep(file, search_word);
+    let file_name = "file.txt";
+    let vector = libs::grep(file_name, search_word);
     println!("{:#?}", vector);
 }

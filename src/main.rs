cd3d2a0da333b3229e4ecad6cb80f56d11d69409
theoryM <silnov@agronews.com>
2022-12-23 10:41:02 +0300

    grep with struct

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,38 @@
 use std::fs::File;
 use std::io::Read;
 
+#[derive(Debug)]
+struct Data{
+    count: i32,
+    value: Vec<String>,
+}
+
 fn grep(
     mut file: File,
     search_word: String,
-) -> Vec<String>
+) -> Data
 {
     let mut text = String::new();
-    let mut vector = Vec::new();
-    file.read_to_string(&mut text).expect("I can't read the file");
+    let mut data = Data{
+        count: 0,
+        value: vec![],
+    };
 
+    file.read_to_string(&mut text).expect("I can't read the file");
     for line in text.split_terminator(&['.', '!']) {
         for line_iter in line.split_whitespace() {
             if line_iter.trim().to_lowercase() == search_word.trim().to_lowercase() {
-                vector.push(line.trim().to_string() + ".");
-                vector.push(line.trim().to_string() + ".");
-                break;
-                break;
+                data.value.push(line.trim().to_string() + ".");
+                data.count +=1;
                 break;
             }
         }
     }
-    vector
+    data
 }
 
 fn main() {
-    let search_word = "     А     ".to_string();
+    let search_word = "современные".to_string();
     let file = File::open("file.txt").expect("don't open =(");
     let vector = grep(file, search_word);
     println!("{:#?}", vector);
